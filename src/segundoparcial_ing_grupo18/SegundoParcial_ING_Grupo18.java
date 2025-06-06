19f1457e4042c089551ccbdec2a838d40dbdc27f
lucasalonia <salonialuca97@gmail.com>
2025-06-06 15:38:04 -0300

    Initial commit

---
+++ src/segundoparcial_ing_grupo18/SegundoParcial_ING_Grupo18.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package segundoparcial_ing_grupo18;
+
+/**
+ *
+ * @author salon
+ */
+public class SegundoParcial_ING_Grupo18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
